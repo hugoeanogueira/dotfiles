c2f45bf7c0d753b4e4b7fe4c00e7cc948bfb1e61
Hugo Nogueira <hugoeanogueira@gmail.com>
2016-10-11 17:34:05 +0100

    Update docker.

--- _scripts/install_6_apps.sh
+++ _scripts/install_6_apps.sh
@@ -47,7 +47,7 @@ install () {
     brew cask install cord;
     brew cask install diskmaker-x;
     brew cask install diskwave;
-    brew cask install dockertoolbox;
+    brew cask install docker;
     brew cask install dropbox;
     brew cask install filezilla;
     brew cask install firefox;
